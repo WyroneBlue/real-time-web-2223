0ae28f4e38ad4703d3890d8e0dea8d1868a424b3
WyroneBlue <wyroneblue@gmail.com>
2023-04-21 09:56:05 +0200

    Added order routes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ import { Router } from 'express';
 const router = Router();
 
 import home from './home.js';
+import order from './order.js';
 
 router.use('/', home);
+router.use('/orders', order);
 
 export default router;
