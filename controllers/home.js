a614cc39f114bc131f39470a4df8255c8ab43429
WyroneBlue <wyroneblue@gmail.com>
2023-04-21 16:19:43 +0200

    updated home page

--- controllers/home.js
+++ controllers/home.js
@@ -4,7 +4,34 @@ export const index = async(req, res) => {
         title: "Home"
     };
 
+    const items = [
+        {
+            name: "Pizza",
+            price: 10,
+        },
+        {
+            name: "Burger",
+            price: 5,
+        },
+        {
+            name: "Hot Dog",
+            price: 3,
+        },
+        {
+            name: "Fries",
+            price: 2,
+        },
+    ];
+
+    const food = items.map(item => {
+        return {
+            ...item,
+            slug: item.name.toLowerCase().replace(/ /g, "-"),
+        };
+    });
+
     res.status(200).render('home', {
         page,
+        food,
     });
 };
--- resources/js/order-form.js
+++ resources/js/order-form.js
@@ -2,30 +2,49 @@ console.log("Wagwan from order.js!");
 const socket = io();
 
 const form = document.querySelector('form');
-const button = document.querySelector('form button');
-console.log(button);
+const addButtons = document.querySelectorAll('form label button');
+console.log(addButtons);
 
-button.addEventListener('click', async(e) => {
-    e.preventDefault();
-    console.log("Wagwan from order.js!");
-    console.log(form);
-    const items = [
-        {
-            name: 'Pizza',
-            quantity: 2
-        },
-        {
-            name: 'Burger',
-            quantity: 1
+const submitButton = document.querySelector('form > button');
+
+let order = [];
+addButtons.forEach(button => {
+    button.addEventListener('click', async(e) => {
+        e.preventDefault();
+
+        e.target.disabled = true;
+        setTimeout(() => {
+            e.target.disabled = false;
+        }, 1000);
+
+        const input = e.target.closest('label').querySelector('input');
+
+        const isDuplicate = order.find(item => item.name === input.name);
+        if(isDuplicate) {
+            isDuplicate.quantity = parseInt(isDuplicate.quantity) + parseInt(input.value);
+            console.log('order', order);
+            return;
+        } else {
+            order.push({
+                name: input.name,
+                quantity: input.value
+            });
         }
-    ]
+        input.value = 1;
 
+        console.log('order', order);
+        console.dir('form', form)
+    });
+});
+
+const submitOrder = async(e) => {
     // wait for order to save
-    setTimeout(async() => {
+    setTimeout(async () => {
         console.log('Order saved!');
         await socket.emit('new-order', {
-            items,
+            order,
             table: 39
         });
     }, 1000);
-});
+}
+
