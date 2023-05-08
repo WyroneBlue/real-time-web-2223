012b6ce7e249d26147f098533232030a6fec9107
WyroneBlue <wyroneblue@gmail.com>
2023-05-08 18:00:39 +0200

    added loadingscreen

---
+++ components/LoadingScreen.vue
@@ -0,0 +1,55 @@
+<script setup>
+const props = defineProps({
+    loading: Boolean,
+});
+</script>
+<template>
+    <Transition name="loading">
+        <aside v-if="loading">
+            <span></span>
+        </aside>
+    </Transition>
+</template>
+
+<style lang="scss" scoped>
+aside {
+    position: fixed;
+    inset: 0;
+
+    display: grid;
+    place-items: center;
+    z-index: 9999;
+    background-color: lightblue;
+
+    span {
+        width: 100px;
+        height: 100px;
+        border-radius: 30px;
+        border-top: 30px solid lightcoral;
+        border-bottom: 20px solid lightyellow;
+        animation: spin 1s ease-in-out infinite;
+    }
+}
+
+@keyframes spin {
+    0% {
+        transform: rotate(0deg);
+    }
+
+    100% {
+        transform: rotate(360deg);
+    }
+}
+
+.loading-enter-active,
+.loading-leave-active {
+    transition: all 0.4s;
+}
+
+.loading-enter-from,
+.loading-leave-to {
+    opacity: 0;
+    scale: 0;
+    filter: blur(2px);
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,3 @@
-<template>
-    <!-- header -->
-    <HeaderNav />
-    <main>
-        <slot />
-    </main>
-</template>
-
 <script setup>
 useHead({
     title: 'Restaurant Management',
@@ -13,8 +5,26 @@ useHead({
         { name: 'description', content: 'Manage your restaurant' }
     ],
 })
+
+const loading = ref(true);
+onMounted(() => {
+    setTimeout(() => {
+        console.log('loading done');
+        // loading.value = false;
+    }, 2000);
+
+    loading.value = false;
+})
 </script>
 
+<template>
+    <!-- header -->
+    <HeaderNav />
+    <main>
+        <slot />
+    </main>
+    <LoadingScreen :loading="loading"/>
+</template>
 
 <style scoped>
 .router-link-exact-active {
